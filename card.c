d02b0300803999190e8b3d4ab653e1d61a6e43e7
Vidan Kukic <kukicvidan@gmail.com>
2025-03-15 01:34:30 +0100

    formating

--- card.c
+++ card.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int main(void){
+int main(void) {
 
   long card_number;
 
@@ -8,34 +8,33 @@ int main(void){
   scanf("%ld", &card_number);
   int count = 0;
   long temp = card_number;
-	while (temp > 0){
-	  temp /= 10;
-	  count++;
-	}
-
-printf("Youre card have %d numubers in it\n ", count);
-printf("Witch means.......  .......  .......\n");
-
-long temp2 = card_number;
-  while (temp2 >= 100){
-	temp2 /=10;
+  while (temp > 0) {
+    temp /= 10;
+    count++;
   }
-long temp3 = card_number;
-  while (temp3 >=10){
-	  temp3 = temp3 / 10;
-  	}
 
-if (temp2 == 51 ||temp2 == 52 || temp2 == 53 || temp2 == 54 || temp2 == 55){
-	printf("I guess it is MasterCard?\n");
+  printf("Youre card have %d numubers in it\n ", count);
+  printf("Witch means.......  .......  .......\n");
 
-}else if (temp2 == 34 || temp2 == 37){
-		printf("I guess it is American Express\n");
-}else if (temp3 == 4) {
-	printf("It is probbably Visa and it starts with number 4?\n");
-}else {
-	printf("Have no fcking idea....");
-}
+  long temp2 = card_number;
+  while (temp2 >= 100) {
+    temp2 /= 10;
+  }
+  long temp3 = card_number;
+  while (temp3 >= 10) {
+    temp3 = temp3 / 10;
+  }
+
+  if (temp2 == 51 || temp2 == 52 || temp2 == 53 || temp2 == 54 || temp2 == 55) {
+    printf("I guess it is MasterCard?\n");
+
+  } else if (temp2 == 34 || temp2 == 37) {
+    printf("I guess it is American Express\n");
+  } else if (temp3 == 4) {
+    printf("It is probbably Visa and it starts with number 4?\n");
+  } else {
+    printf("Have no fcking idea....");
+  }
 
   return 0;
-
 }
